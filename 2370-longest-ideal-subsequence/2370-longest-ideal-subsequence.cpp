774e90f1189dacac18e915762ea96d4d7b7c9f80
vaibhav dahiya <dahiyavaibhav89@gmail.com>
2022-09-05 19:53:41 -0700

    Time: 633 ms (35.13%), Space: 14 MB (40.83%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    
-        int longestIdealString(string s, int k) {
-        int DP[26] = {0}, ans = 1;
-        
-        for (char &ch: s) {
-            int i = ch - 'a';
-            DP[i] = DP[i] + 1;
+    int longestIdealString(string s, int k) {
+        //
+        vector<int> len(s.size()+1, 0);
+        int res = 0;
+        vector<int> alpha(26, 0);
+        for(int i = 0; i < s.size(); i++) {
             
-            for (int j = max(0, i - k); j <= min(25, i + k); j++)
-                if (j != i)
-                    DP[i] = max(DP[i], DP[j] + 1);
+            int now = s[i] -'a';
+            len[i] = max(alpha[now]+1, len[i]);
+            alpha[now] = max(len[i], alpha[now]);
             
-            ans = max(ans, DP[i]);
+            res = max( len[i], res);
+            
+            for(int j = 1; j <= k; j++) {
+                    if(now+j < 26) {
+                        len[i] = max(alpha[now+j]+1, len[i]);
+                        alpha[s[i]-'a'] = max(len[i], alpha[s[i]-'a']) ;
+                        res = max(len[i], res);
+                    }
+                    if(now -j >= 0) {
+                        len[i] = max(alpha[now-j]+1, len[i]);
+                        alpha[s[i]-'a'] = max(len[i], alpha[s[i]-'a']) ;
+                        res = max(len[i], res);
+                    }
+                    
+                }
+            
         }
         
-        return ans;
+        
+        return res;
+        
     }
 };
