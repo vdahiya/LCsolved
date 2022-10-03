746d2b2e37c737e8dbf0117143f8b7a996dbad0f
vaibhav dahiya <dahiyavaibhav89@gmail.com>
2022-10-02 17:11:19 -0700

    Time: 2026 ms (13.68%), Space: 588.2 MB (5.21%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -1,43 +1,65 @@
-struct TrieNode {
-    TrieNode *next[26] = {};
-    int cnt = 0;
-};
-
 class Solution {
-    TrieNode root;
 public:
-    void insert(string word) {
-        TrieNode * node = &root;
-        for(char c : word) {
-            if(!node->next[c - 'a']) {
-                node->next[c - 'a'] = new TrieNode();
-            } 
-            node->next[c - 'a']->cnt++;
-            node = node->next[c - 'a'];   
+    
+    struct trie {
+       
+        vector<shared_ptr<trie>> next;
+        int score;
+       
+        trie() {
+            next.resize(26, nullptr);          
+            this->score = 0;
         }
+    };
+    
+    void insert(string &s, shared_ptr<trie>  root) {
+        
+        shared_ptr<trie> it = root;
+        
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(!it->next[s[i]-'a']) {
+                auto here = make_shared<trie>();
+                it->next[s[i]-'a'] = here;
+            }
+            it->next[s[i]-'a']->score+=1;
+            it = it->next[s[i]-'a'];
+        }
+        
+        
+      
     }
-
-    int prefixCnt(string s) {
-        TrieNode *node = &root;
-        int ans = 0;
-        for(char c : s) {
-            ans += node->next[c - 'a']->cnt;
-            node = node->next[c - 'a'];
+    
+    int get_score(shared_ptr<trie> & root, string &s) {
+        
+        int sc= 0;
+        shared_ptr<trie> next = root;
+        
+        for(int i = 0; i < s.size(); i++) {
+            
+            sc += next->next[s[i]-'a']->score;
+            next = next->next[s[i]-'a'];
         }
-        return ans;
+        return sc;
     }
     
     vector<int> sumPrefixScores(vector<string>& words) {
-        int N = words.size();
-		// Insert words in trie.
-        for (int i = 0; i < N; i++) {
-                insert(words[i]);
+        
+        shared_ptr<trie> root = make_shared<trie>();
+        //cout<<root<<endl;
+        shared_ptr<trie> &orig = root;
+        for(auto &k:words) {
+            //cout<<root<<endl;
+            insert(k, root);
         }
-        vector<int> ans(N, 0);
-        for (int i = 0; i < N; i++) {
-			// Get the count of all prefixes of given string.
-            ans[i] = prefixCnt(words[i]); 
+        
+        vector<int> res;
+        
+        for(auto &k:words) {
+            res.push_back(get_score(root, k));
         }
-        return ans;
+        
+        
+        return res;
     }
 };
